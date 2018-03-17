1fc624476a265d191e45e9d232a34dac629081e2
Andrew67 <andres@andrew67.com>
2018-03-17 01:09:45 -0400

    Mark OkHttpClient/JsonAdapter as final.

--- app/src/main/java/com/andrew67/ddrfinder/adapters/MapLoaderV3.java
+++ app/src/main/java/com/andrew67/ddrfinder/adapters/MapLoaderV3.java
@@ -51,8 +51,8 @@ import java.util.Set;
 
 public class MapLoaderV3 extends MapLoader {
 
-    private static OkHttpClient client = new OkHttpClient();
-    private static JsonAdapter<Result> jsonAdapter = new Moshi.Builder().build().adapter(Result.class);
+    private static final OkHttpClient client = new OkHttpClient();
+    private static final JsonAdapter<Result> jsonAdapter = new Moshi.Builder().build().adapter(Result.class);
 
     public MapLoaderV3(ClusterManager<ArcadeLocation> clusterManager,
                        List<ArcadeLocation> loadedLocations, Set<Integer> loadedArcadeIds,
